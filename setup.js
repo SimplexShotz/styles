5b0ee371a5bc5d0d7e31270107e8fe1a748c4bcb
SimplexShotz <44213379+SimplexShotz@users.noreply.github.com>
2019-09-27 11:11:29 -0400

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-  alert(window.location.search);
+  // alert(window.location.search);
   document.head.innerHTML += "<link rel='stylesheet' href='https://simplexshotz.github.io/styles/basic.css'>";
   document.head.innerHTML += "<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,500,700,900&display=swap'>";
   document.head.innerHTML += "<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Ubuntu:700&display=swap'>";
