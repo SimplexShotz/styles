d623f023f9802f067044bc38c8dd2d99867f70ef
SimplexShotz <44213379+SimplexShotz@users.noreply.github.com>
2019-09-27 10:31:22 -0400

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,2 +1,4 @@
-// alert(window.location.search);
-document.head.innerHTML += "<link rel='stylesheet' href='https://simplexshotz.github.io/styles/basic.css'>";
+window.onload = function() {
+  // alert(window.location.search);
+  document.head.innerHTML += "<link rel='stylesheet' href='https://simplexshotz.github.io/styles/basic.css'>";
+};
