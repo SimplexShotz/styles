119d3ede14a8045a1a3adf3d63ce10e106154004
SimplexShotz <44213379+SimplexShotz@users.noreply.github.com>
2019-09-27 07:42:24 -0400

    Update setup.js

--- setup.js
+++ setup.js
@@ -2,4 +2,4 @@ alert(window.location.search);
 var ss = document.createElement("link");
 ss.rel = "stylesheet";
 ss.href = "https://simplexshotz.github.io/styles/basic.css";
-document.body.appendChild(s);
+document.head.appendChild(s);
