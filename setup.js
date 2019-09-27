57ee80e6d8145759af59b55c1cbb9ac83a309d17
SimplexShotz <44213379+SimplexShotz@users.noreply.github.com>
2019-09-27 11:10:50 -0400

    Update setup.js

--- setup.js
+++ setup.js
@@ -3,5 +3,5 @@ window.onload = function() {
   document.head.innerHTML += "<link rel='stylesheet' href='https://simplexshotz.github.io/styles/basic.css'>";
   document.head.innerHTML += "<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,500,700,900&display=swap'>";
   document.head.innerHTML += "<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Ubuntu:700&display=swap'>";
-  document.body.innerHTML = "<div id='header'><div id='header-inner'><div class='logo'><div class='logo-tag'>&lt/></div><div class='logo-text'>Brandon</div></div><div id='header-right'>US National Parks</div></div></div><div id='content'>" + document.body.innerHTML + "</div>";
+  document.body.innerHTML = "<div id='header'><div id='header-inner'><div class='logo'><div class='logo-tag'>&lt/></div><div class='logo-text'>Brandon</div></div><div id='header-right'></div></div></div><div id='content'>" + document.body.innerHTML + "</div>";
 };
