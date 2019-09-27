ea9ae9252f51c81905185c6085c6af4260ccdc16
SimplexShotz <44213379+SimplexShotz@users.noreply.github.com>
2019-09-27 07:30:52 -0400

    Create setup.js

---
+++ setup.js
@@ -0,0 +1,5 @@
+alert(window.location.search);
+var ss = document.createElement("link");
+ss.rel = "stylesheet";
+ss.href = "https://simplexshotz.github.io/styles/basic.css";
+document.body.appendChild(s);
