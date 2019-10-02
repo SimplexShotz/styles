d85a383f33ec4eb5cbb3d26511e240d20065d709
SimplexShotz <44213379+SimplexShotz@users.noreply.github.com>
2019-10-02 19:31:35 -0400

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,13 +1,15 @@
 window.onload = function() {
+  var m = "";
   if (/Mobi|Android/i.test(navigator.userAgent)) {
     alert("Your device is a mobile device. This site has not been updated to support mobile devices yet, so it may not display properly.");
+    m = "-mobile";
   }
   var q = getQueries();
   document.head.innerHTML += "<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,500,700,900&display=swap'>";
   document.head.innerHTML += "<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Ubuntu:700&display=swap'>";
   switch (q.type) {
     default:
-      document.head.innerHTML += "<link rel='stylesheet' href='https://styles.simplexshotz.tk/basic.css'>";
+      document.head.innerHTML += "<link rel='stylesheet' href='https://styles.simplexshotz.tk/basic" + m + ".css'>";
       document.body.innerHTML = "<div id='header'><div id='header-inner'><div class='logo'><div class='logo-tag'>&lt/></div><div class='logo-text'>" + (q.name ? q.name.split("_").join(" ") : "Brandon") + "</div></div><div id='header-right'>" + (q.head ? q.head.split("_").join(" ") : document.title) + "</div></div></div><div id='content'>" + document.body.innerHTML + "</div>";
     break;
   }
